square_coil: fixed numerical issue
* Added more documentation.

* Closes #10.

Signed-off-by: Daniel Prelipcean <daniel.prelipcean@tum.de>

setup_elements/elements.py
@@ -11,18 +11,15 @@
 
 import mpmath
 import numpy as np
-import warnings
 
-from physics_constants import MU_0, pi
+from physics_constants import MU_0
 
 
 class BaseCoil:
     """Class that implements basic method and attributes for a coil."""
 
-    MU_0 = MU_0
-
     def __init__(self):
-        self.prefactor = self.MU_0
+        self.prefactor = MU_0
         self.coil_mid_pos = None
         self.length = None
         self.n = None
@@ -62,12 +59,12 @@ class BaseCoil:
         return float(mpmath.ellippi(n, np.pi / 2, m))
 
     @staticmethod
-    def _rotate(vektor, phi, axis):
+    def _rotate(vector, phi, axis):
         """Rotate the vector along an axis for the given angle phi.
 
         Parameters
         ----------
-        vektor: np.array
+        vector: np.array
             3D vector
         phi: float, int
             Angle in radians
@@ -88,7 +85,7 @@ class BaseCoil:
         r = [[n1 ** 2 * (1 - c) + c, n1 * n2 * (1 - c) - n3 * s, n1 * n3 * (1 - c) + n2 * s],
              [n2 * n1 * (1 - c) + n3 * s, n2 ** 2 * (1 - c) + c, n2 * n3 * (1 - c) - n1 * s],
              [n3 * n1 * (1 - c) - n2 * s, n3 * n2 * (1 - c) + n1 * s, n3 ** 2 * (1 - c) + c]]
-        return np.dot(r, vektor)
+        return np.dot(r, vector)
 
     def change_current(self, current):
         """Change the assigned current value."""
@@ -96,6 +93,7 @@ class BaseCoil:
 
     @staticmethod
     def transform_cartesian_to_cylindrical(x, y, z):
+        """Transform coordinates from cartesian to cylindrical."""
         x = x
         rho = np.sqrt(y ** 2 + z ** 2)
         theta = np.arctan(y / z)
@@ -165,8 +163,14 @@ class Coil(BaseCoil):
         return self.prefactor * (self.sum_element_rho(rho, x, s=1) - self.sum_element_rho(rho, x, s=-1))
 
     def sum_element_rho(self, rho, x, s):
+        """Compute the summation for the rho direction.
+
+        Returns
+        -------
+        out: float
+        """
         return self.beta(rho, x, s) / rho \
-               * ((2 - self.m(rho, x, s)) * self._k(self.m(rho, x, s)) - 2 * self._e(self.m(rho, x, s)))
+            * ((2 - self.m(rho, x, s)) * self._k(self.m(rho, x, s)) - 2 * self._e(self.m(rho, x, s)))
 
     def b_field_x(self, x, rho=0):
         """Compute the magnetic field in x direction.
@@ -191,13 +195,17 @@ class Coil(BaseCoil):
 
         n = 4.0 * self.r * rho / (rho + self.r) ** 2
 
-        prefactor = 1e4 * self.MU_0 / (2 * np.pi) * self.n * self.current / self.length
-
-        return prefactor * (-self.sum_element_x(n, rho, x, s=-1) + self.sum_element_x(n, rho, x, s=1))
+        return self.prefactor * (-self.sum_element_x(n, rho, x, s=-1) + self.sum_element_x(n, rho, x, s=1))
 
     def sum_element_x(self, n, rho, x, s):
+        """Compute the summation for the x direction.
+
+        Returns
+        -------
+        out: float
+        """
         return self.zeta(x, s) / self.beta(rho, x, s) \
-               * ((rho - self.r) / (rho + self.r) * self._p(n, self.m(rho, x, s)) - self._k(self.m(rho, x, s)))
+            * ((rho - self.r) / (rho + self.r) * self._p(n, self.m(rho, x, s)) - self._k(self.m(rho, x, s)))
 
     def b_field(self, x, y, z):
         """Compute the magnetic field given the position."""
@@ -214,6 +222,10 @@ class Coil(BaseCoil):
 
 
 class SquareCoil(BaseCoil):
+    """Class that implements a square coil.
+
+    The coordinates from the reference paper are changed from (x, y, z) to (y, z, x), hence the change in the formulae.
+    """
 
     def create_coil(self, coil_mid_pos=0, length=0.1, windings=100, current=10, r=0.05, wire_d=0.006, angle_y=0,
                     angle_z=0):
@@ -234,32 +246,38 @@ class SquareCoil(BaseCoil):
         self.angle_y = angle_y
         self.angle_z = angle_z
 
-        self.prefactor *= 1 / (4 * np.pi) * self.n * self.current / self.length
+        self.prefactor *= 1e4 / (4 * np.pi) * self.n * self.current / self.length
 
     def b_field(self, x, rho, theta):
-        """Compute the magnetic field."""
+        """Compute the magnetic field.
+
+        Parameters
+        ----------
+        position in cylindrical coordinates
+
+        Returns
+        -------
+        magnetic field in cartesian coordinates
+        """
         self.x, self.y, self.z = self.transform_cylindrical_to_cartesian(x, rho, theta)
 
-        # self.x -= self.coil_mid_pos
+        self.x -= self.coil_mid_pos
 
-        field = self.prefactor * np.array([self.eta_x, self.eta_y, self.eta_z])
+        field = self.prefactor * np.array([self.sum_element_x, self.sum_element_y, self.sum_element_z])
 
-        return self._fix_numerical_error(field)
+        return self.sanitize_output(field)
 
     @staticmethod
-    def _fix_numerical_error(field):
-        """Fix computational error.
+    def sanitize_output(field):
+        """Sanitize magnetic field..
+
+        Remove possible numerical singularities.
 
         Parameters
         ----------
         field: np.array
             3D vector
         """
-
-        # if field[0] > 0:
-        #     field[0] = 0
-        # elif field[0] < 0:
-        #     field[0] = - field[0]
 
         if any(np.isinf(field)) or any(np.isnan(field)):
             field = np.array([0, 0, 0])
@@ -282,79 +300,80 @@ class SquareCoil(BaseCoil):
     #     return  self.eta_z
 
     @property
-    def eta_x(self):
+    def sum_element_x(self):
+        """Compute the summation for the x direction.
+
+        Returns
+        -------
+        out: float
+        """
         _eta = 0
         for alpha in np.arange(1, 5):
             alpha_index = alpha - 1
 
-            with warnings.catch_warnings(record=True) as w:
-                # Cause all warnings to always be triggered.
-                warnings.simplefilter("always")
-                # Verify some things
-                t1 = ((-1) ** alpha) * self.d[alpha_index] / \
-                     (self.r_values[alpha_index] * (self.r_values[alpha_index]
-                                                    + (-1) ** (alpha + 1) * self.c[alpha_index]))
-                t2 = self.c[alpha_index] \
-                     / (self.r_values[alpha_index] * (self.r_values[alpha_index] + self.d[alpha_index]))
-                if t1 == 0 or t2 == 0:
-                    print(f"0 value at {self.x}, {self.y}, {self.z}")
+            t1 = ((-1) ** alpha) * self.d[alpha_index] / \
+                 (self.r_values[alpha_index] * (self.r_values[alpha_index]
+                                                + (-1) ** (alpha + 1) * self.c[alpha_index]))
+            t2 = self.c[alpha_index] / (self.r_values[alpha_index] * (self.r_values[alpha_index] + self.d[alpha_index]))
 
-                _eta += t1 - t2
+            _eta += t1 - t2
 
-                if len(w) == 1:
-                    # ToDo: what to do when dividing by 0?
-                    print(f"Numerical error encountered at {self.x, self.y, self.z}")
-                    _eta = 0
         return _eta
 
     @property
-    def eta_y(self):
+    def sum_element_y(self):
+        """Compute the summation for the y direction.
+
+        Returns
+        -------
+        out: float
+        """
         _eta = 0
         for alpha in np.arange(1, 5):
             alpha_index = alpha - 1
 
-            _eta += ((-1)**(alpha+1)) * self.z\
-                    /(self.r_values[alpha_index] * (self.r_values[alpha_index]
-                                                    + ((-1) ** (alpha+1)) * self.c[alpha_index]))
+            _eta += ((-1)**(alpha+1)) * self.z / (self.r_values[alpha_index] * (self.r_values[alpha_index]
+                                                  + ((-1) ** (alpha+1)) * self.c[alpha_index]))
         return _eta
 
     @property
-    def eta_z(self):
+    def sum_element_z(self):
+        """Compute the summation for the z direction.
+
+        Returns
+        -------
+        out: float
+        """
         _eta = 0
         for alpha in np.arange(1, 5):
             alpha_index = alpha - 1
 
-            with warnings.catch_warnings(record=True) as w:
-                _eta += ((-1) ** (alpha + 1)) * self.z / (
-                            self.r_values[alpha_index] * (self.r_values[alpha_index] + self.d[alpha_index]))
-                if len(w) == 1:
-                    # ToDo: what to do when dividing by 0?
-                    print(f"Numerical error encountered at {self.x, self.y, self.z}")
-                    _eta = 0
+            _eta += ((-1) ** (alpha + 1)) * self.z / (
+                        self.r_values[alpha_index] * (self.r_values[alpha_index] + self.d[alpha_index]))
 
         return _eta
 
     @property
     def c(self):
-        return np.array([self.a + self.x,
-                         self.a - self.x,
-                         -self.a + self.x,
-                         -self.a - self.x])
+        return np.array([self.a + self.y,
+                         self.a - self.y,
+                         -self.a + self.y,
+                         -self.a - self.y])
 
     @property
     def d(self):
-        return np.array([self.b + self.y,
-                         self.b + self.y,
-                         self.y - self.b,
-                         self.y - self.b])
+        return np.array([self.b + self.z,
+                         self.b + self.z,
+                         self.z - self.b,
+                         self.z - self.b])
 
     @property
     def r_values(self):
         return np.array([
-            np.sqrt((self.a + self.x) ** 2 + (self.b + self.y) ** 2 + self.z ** 2),
-            np.sqrt((self.a - self.x) ** 2 + (self.b + self.y) ** 2 + self.z ** 2),
-            np.sqrt((self.a - self.x) ** 2 + (self.b - self.y) ** 2 + self.z ** 2),
-            np.sqrt((self.a + self.x) ** 2 + (self.b - self.y) ** 2 + self.z ** 2)]
+            np.sqrt((self.a + self.y) ** 2 + (self.b + self.z) ** 2 + self.x ** 2),
+            np.sqrt((self.a - self.y) ** 2 + (self.b + self.z) ** 2 + self.x ** 2),
+            np.sqrt((self.a - self.y) ** 2 + (self.b - self.z) ** 2 + self.x ** 2),
+            np.sqrt((self.a + self.y) ** 2 + (self.b - self.z) ** 2 + self.x ** 2)]
         )
 
 
@@ -379,24 +398,39 @@ class RealCoil(BaseCoil):
 
         self.prefactor *= 1e4 / (2 * np.pi) * self.n * self.current / self.length
 
-    def b_field_rho(self, x, r, rho):
-        """ # radial magnetic field
+    def b_field_rho(self, x, r, axis_point):
+        """Compute the magnetic field in rho (radial) direction.
 
+
+        Parameters
+        ----------
+        x: float
+            Value on the x axis, along the coil.
+        r: float
+            Radius value.
+        axis_point: float
+            Value on the y or z axis.
         """
         x -= self.coil_mid_pos
-        if rho == 0:
+        if axis_point == 0:
             return 0
 
-        rho = abs(rho)
+        if r:
+            # ToDo: is the radius really not playing a role?
+            pass
+
+        rho = abs(axis_point)
 
         return self.prefactor * (self.sum_element_rho(rho, x, s=1) - self.sum_element_rho(rho, x, s=-1))
 
     def sum_element_rho(self, rho, x, s):
-        return self.beta(rho, x, s) / rho \
-               * ((2 - self.m(rho, x, s)) * self._k(self.m(rho, x, s)) - 2 * self._e(self.m(rho, x, s)))
+        """Compute the summation for the rho direction."""
+        return self.beta(rho, x, s) \
+            / rho * ((2 - self.m(rho, x, s)) * self._k(self.m(rho, x, s)) - 2 * self._e(self.m(rho, x, s)))
 
     def b_field_x(self, x, r, rho=0):
-        """
+        """Compute the magnetic field in the x direction.
+
         # N = turns of winding, I = current (A), R = radius (m), l = length (m)
         # x für mich y
 
@@ -407,13 +441,24 @@ class RealCoil(BaseCoil):
 
         n = 4.0 * r * rho / (rho + r) ** 2
 
-        prefac = 1e4 * self.MU_0 / (2 * np.pi) * self.n * self.current / self.length
-
-        return prefac * (-self.sum_element_x(n, rho, x, s=-1) + self.sum_element_x(n, rho, x, s=1))
+        return self.prefactor * (-self.sum_element_x(n, rho, x, s=-1) + self.sum_element_x(n, rho, x, s=1))
 
     def sum_element_x(self, n, rho, x, s):
+        """Compute the summation for the x direction.
+
+        Parameters
+        ----------
+        n: int
+            Winding number
+        rho: float
+            Radius value.
+        x: float
+            Position on the x axis along the coil.
+        s: [-1, 1]
+            Boundary values for hte elliptical functions
+        """
         return self.zeta(x, s) / self.beta(rho, x, s) \
-               * ((rho - self.r) / (rho + self.r) * self._p(n, self.m(rho, x, s)) - self._k(self.m(rho, x, s)))
+            * ((rho - self.r) / (rho + self.r) * self._p(n, self.m(rho, x, s)) - self._k(self.m(rho, x, s)))
 
     def b_field(self, x, y, z):
         """Compute the magnetic field given the position."""