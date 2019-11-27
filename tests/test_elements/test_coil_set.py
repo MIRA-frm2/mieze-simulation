2c0b2feefc46aec9793e231026ea01a0ee5ff863
Daniel Prelipcean <dprelipcean@jacobs-alumni.de>
2019-11-27 15:01:39 +0100

    tests: added simple execution for the CoilSet
    Signed-off-by: Daniel Prelipcean <dprelipcean@jacobs-alumni.de>

---
+++ tests/test_elements/test_coil_set.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from elements.coil_set import CoilSet
+
+
+def simple_execution():
+    coil_set = CoilSet(position=0, distance_12=0, distance_34=0)
+
+    # Computational grid space
+    startpoint = -0.25  # [m]
+    endpoint = 0.5  # [m]  # Positions.get_position_coilA()
+    npoints = 100
+    x_positions = np.linspace(startpoint, endpoint, num=npoints)
+
+    # Compute B_field_values
+    b_field_values = coil_set.compute_b_field(x_positions)
+
+    plt.plot(x_positions, b_field_values)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    simple_execution()
